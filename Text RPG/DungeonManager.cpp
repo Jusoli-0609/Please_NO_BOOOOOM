4e0989a994f887c8b0987781f03e6833c5fc5f84
이미르 <2mir0012@gmail.com>
2026-08-04 00:57:02 +0900

    Revert "던전매니저 신규"
    This reverts commit 3364b74cafd8b326e58447c024ad1a07b08fdf6b.

--- Text RPG/DungeonManager.cpp
+++ Text RPG/DungeonManager.cpp
@@ -1,4 +1,4 @@
-﻿#include "DungeonManager.h"
+#include "DungeonManager.h"
 #include "Battle_System.h"
 #include "Battle_Elite_Skill.h"
 
@@ -411,6 +411,7 @@ void Dungeon_Manager::Get_Current_Chapter_Monsters(Monster_Type monster_Types[])
 		monster_Types[1] = Monster_Type::BOOL_MUSHROOM;
 		monster_Types[2] = Monster_Type::IF_GOBLIN;
 		break;
+	 
 	case Chapter_Type::ARRAY_LOOP_OCEAN:
 		monster_Types[0] = Monster_Type::ARRAY_JELLYFISH;
 		monster_Types[1] = Monster_Type::FOR_SHARK;
@@ -454,9 +455,56 @@ Monster_Type Dungeon_Manager::Get_Random_Normal_Monster() const
 
 bool Dungeon_Manager::Check_Elite_Monster_Appearance()
 {
-	if (_has_Elite_Appeared_In_Current_Chapter)
+	int appearance_Roll = rand() % 100 + 1;
+
+	return
+		appearance_Roll <= ELITE_APPEARANCE_RATE;
+}
+// 5-2. 챕터별 정예 문제 랜덤 선택
+Elite_Question Dungeon_Manager::Get_Elite_Question(Chapter_Type chapter_Type) const
+{
+	Elite_Question elite_Questions[3];
+
+	switch (chapter_Type)
 	{
-		return false;
+	case Chapter_Type::VARIABLE_CONDITION_FOREST:
+	{
+		elite_Questions[0].question = "다음 중 정수값을 저장하는 자료형은 무엇인가?";
+
+		elite_Questions[0].choices[0] = "int";
+		elite_Questions[0].choices[1] = "bool";
+		elite_Questions[0].choices[2] = "if";
+		elite_Questions[0].choices[3] = "while";
+
+		elite_Questions[0].correct_Answer = 1;
+
+
+		elite_Questions[1].question =
+			"다음 코드에서 출력되는 결과는 무엇인가?\n"
+			"int number = 10;\n"
+			"if (number > 5)\n"
+			"{\n"
+			"\tcout << \"통과\";\n"
+			"}";
+
+		elite_Questions[1].choices[0] = "아무것도 출력되지 않음";
+		elite_Questions[1].choices[1] = "통과";
+		elite_Questions[1].choices[2] = "10";
+		elite_Questions[1].choices[3] = "오류 발생";
+
+		elite_Questions[1].correct_Answer = 2;
+
+
+		elite_Questions[2].question = "참 또는 거짓만 저장할 수 있는 자료형은 무엇인가?";
+
+		elite_Questions[2].choices[0] = "int";
+		elite_Questions[2].choices[1] = "string";
+		elite_Questions[2].choices[2] = "bool";
+		elite_Questions[2].choices[3] = "float";
+
+		elite_Questions[2].correct_Answer = 3;
+
+		break;
 	}
 
 	int appearance_Roll = rand() % 100 + 1;
@@ -472,7 +520,7 @@ bool Dungeon_Manager::Check_Elite_Monster_Appearance()
 }
 
 bool Dungeon_Manager::Run_Elite_Quiz(Player* player,Monster& elite_Monster)
-{
+{ 
 	if (player == nullptr)
 	{
 		return false;
@@ -606,7 +654,10 @@ void Dungeon_Manager::Apply_Tutor_Gimmick_Failure_Penalty()
 void Dungeon_Manager::Record_Monster_Kill(const Monster& monster)
 {
 	Monster_Type monster_Type = monster.getMonsterType();
-	Monster_Kill_Record& kill_Record = _monster_Kill_Log[_current_Chapter][monster_Type];
+
+	Monster_Kill_Record& kill_Record = _monster_Kill_Log
+		[_current_Chapter]
+		[monster_Type];
 
 	if (kill_Record.monster_Name.empty())
 	{
@@ -1094,7 +1145,7 @@ void Dungeon_Manager::Run_Final_Boss_Room(Player* player, Inventory<Item>& inven
 	kim_Dong_Hyun_Manager.Print_Monster_Info();
 	kim_Dong_Hyun_Manager.Print_Attack_Message();
 
-	Battle(player, kim_Dong_Hyun_Manager,inventory);
+	Boss_Battle(player, kim_Dong_Hyun_Manager,inventory);
 
 	if(player->Get_Hp() <= 0 || kim_Dong_Hyun_Manager.getHP() > 0)
 	{
@@ -1125,7 +1176,7 @@ void Dungeon_Manager::Run_Final_Boss_Room(Player* player, Inventory<Item>& inven
 	moon_Seung_Ho_Manager.Print_Monster_Info();
 	moon_Seung_Ho_Manager.Print_Attack_Message();
 
-	Battle(player, moon_Seung_Ho_Manager,inventory);
+	Boss_Battle(player, moon_Seung_Ho_Manager,inventory);
 
 	if
 		(player->Get_Hp() <= 0 || moon_Seung_Ho_Manager.getHP() > 0)
