3c298f5ba0e036fd00fa308e52bb427dbb8ca50a
Yoonjae Jung <yoonjae01072501510@gmail.com>
2026-08-04 01:50:29 +0900

    4532

--- Text RPG/Craft_Item.cpp
+++ Text RPG/Craft_Item.cpp
@@ -1,168 +1,73 @@
-#include "Craft_Item.h"
+#include "Craft_Work_Shop.h"
+
 #include <iostream>
 
 using namespace std;
 
-
-
 Craft_Work_Shop::Craft_Work_Shop()
 {
-
+    // TODO: recipe_repository 초기화 코드
+    // 예: 기본 레시피 등록이 필요하다면 이 위치에 작성
 }
 
-
-
-// 모든 레시피 출력
 void Craft_Work_Shop::Print_All_Recipes() const
 {
+    // TODO: Recipe_Repository의 전체 레시피 출력 함수 호출 코드
 
-    vector<All_Recipes> recipes =
-        recipe_repository.Get_All_Recipes();
-
-
-    cout << "\n===== 제작 가능한 레시피 =====\n";
-
-
-    for (int i = 0; i < recipes.size(); i++)
-    {
-        cout << "\n[" << i + 1 << "번]\n";
-
-        recipes[i].Print_Info();
-    }
-
+    // 예시 형태:
+    // recipe_repository.Print_All_Recipes();
 }
 
-
-
-// 아이템 제작
 bool Craft_Work_Shop::Craft_Item(
     Inventory<Item>& inventory
 )
 {
+    // TODO: 제작할 레시피 선택을 위한 입력 변수 선언
 
-    vector<All_Recipes> recipes =
-        recipe_repository.Get_All_Recipes();
+    // TODO: 선택한 레시피 조회 코드
 
+    // TODO: 인벤토리에 재료가 충분한지 확인하는 조건문
 
-    if (recipes.empty())
-    {
-        cout << "등록된 레시피가 없다!" << endl;
-        return false;
-    }
+    // TODO: 재료 차감 코드
 
+    // TODO: 제작 결과 아이템 추가 코드
 
+    // TODO: 제작 성공 여부 반환값
+    return false;
+}
 
-    Print_All_Recipes();
+bool Craft_Work_Shop::Decomposition_Item(
+    Inventory_For_Equipments_Only& equipment_inventory,
+    Inventory<Item>& item_inventory
+)
+{
+    // TODO: 분해할 장비 선택을 위한 입력 변수 선언
 
+    // TODO: 장비 인벤토리에서 대상 장비 조회 코드
 
-    int choice;
+    // TODO: 분해 가능 여부 확인 조건문
 
-    cout << "제작할 아이템 번호 : ";
-    cin >> choice;
+    // TODO: 장비 제거 코드
 
+    // TODO: 분해 결과 아이템 추가 코드
 
+    // TODO: 분해 성공 여부 반환값
+    return false;
+}
 
-    if (choice < 1 || choice > recipes.size())
-    {
-        cout << "잘못된 선택이다!" << endl;
-        return false;
-    }
+bool Craft_Work_Shop::Enhance_Item(
+    Equipment& equipment
+)
+{
+    // TODO: 강화에 필요한 데이터 변수 선언
+    // 예: 강화 확률, 비용, 현재 강화 수치 등
 
+    // TODO: 강화 가능 여부 확인 조건문
 
+    // TODO: 강화 성공/실패 판정에 필요한 코드 위치
 
-    All_Recipes selected_recipe =
-        recipes[choice - 1];
+    // TODO: 성공 시 장비 능력치 변경 코드
 
-
-
-    // 재료 보유 확인
-
-    for (auto ingredient :
-        selected_recipe.Get_Ingredients())
-    {
-
-        bool found = false;
-
-
-        for (int i = 0; i < inventory.Get_Size(); i++)
-        {
-
-            Item* item =
-                inventory.Get_Item_By_Index(i);
-
-
-            if (item->_Item_Name ==
-                ingredient.first)
-            {
-
-                if (item->_Item_Count >= ingredient.second)
-                {
-                    found = true;
-                }
-
-            }
-
-        }
-
-
-
-        if (found == false)
-        {
-            cout
-                << ingredient.first
-                << "이(가) 부족하다!"
-                << endl;
-
-            return false;
-        }
-
-    }
-
-
-
-    // 재료 제거
-
-    for (auto ingredient :
-        selected_recipe.Get_Ingredients())
-    {
-
-        inventory.Remove_Item_Count(
-            ingredient.first,
-            ingredient.second
-        );
-
-    }
-
-
-
-    // 결과 아이템 생성
-
-    Item result;
-
-
-    result._Item_Name =
-        selected_recipe.Get_Recipe_Name();
-
-
-    result._Item_Count = 1;
-
-
-    result._Item_Description =
-        "제작된 아이템";
-
-
-
-    inventory.Add_Or_Increase_Item(result);
-
-
-
-    cout
-        << result._Item_Name
-        << " 제작 성공!"
-        << endl;
-
-
-
-    return true;
-
+    // TODO: 강화 성공 여부 반환값
+    return false;
 }
