fbcac775b9af41ba15c34f1821dbe90759b00573
hwa0907194-ui <hwa0907194@gmail.com>
2026-08-04 12:44:33 +0900

    버그수정

--- Text RPG/Game_Manager.h
+++ Text RPG/Game_Manager.h
@@ -28,8 +28,5 @@ private:
     Currently_Equipped_Equipments _Currently_Equipped_Equipments;
 	Currently_Equipped_Tutor _Currently_Equipped_Tutor;
     Inventory_For_Equipments_Only _Equipment_Inventory;
-    Currently_Equipped_Equipments _Currently_Equipped_Equipments;
-    Currently_Equipped_Equipments _Currently_Equipped_Equipments;
-    Currently_Equipped_Tutor _Currently_Equipped_Tutor;
     Player* _Player;
 };
