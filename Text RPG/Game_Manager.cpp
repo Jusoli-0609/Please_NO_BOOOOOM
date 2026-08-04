ed52aaa6edbb31c33e4b7d5c1aff513a0a94f44b
이미르 <2mir0012@gmail.com>
2026-08-04 10:23:13 +0900

    올블루!!!

--- Text RPG/Game_Manager.cpp
+++ Text RPG/Game_Manager.cpp
@@ -34,17 +34,26 @@ void Game_Manager::Run()
 
 void Game_Manager::Create_Player()
 {
-    int job = Job_Selection();  
+    int job = Job_Selection();
     switch (job)
     {
     case 1:
+    {
         _Player = new JYJ("JYJ");
         break;
+    }
 
     default:
-        cout << "잘못된 직업 선택입니다. 기본 직업으로 시작합니다." << endl;
+    {
+        cout << "잘못된 직업 선택입니다. "  << "기본 직업으로 시작합니다." << endl;
         _Player = new JYJ("JYJ");
         break;
+    }
+    }
+
+    if (_Player != nullptr)
+    {
+        _Player->Set_Status_References(&_Currently_Equipped_Equipments, &_Currently_Equipped_Tutor);
     }
 }
 
@@ -58,7 +67,8 @@ void Game_Manager::Show_Main_Menu()
         cout << "========================================" << endl;
         cout << "1. 던전 입장" << endl;
         cout << "2. 인벤토리" << endl;
-        cout << "3. 내일배움캠프 재정비소" << endl;
+        cout << "3. 캐릭터 정보" << endl;
+        cout << "4. 내일배움캠프 재정비소" << endl;
         cout << "0. 게임 종료" << endl;
         cout << "선택: ";
 
@@ -78,6 +88,17 @@ void Game_Manager::Show_Main_Menu()
         }
 
         case 3:
+        {
+            if (_Player == nullptr)
+            {
+                cout << "캐릭터 정보를 찾을 수 없습니다." << endl;
+                break;
+            }
+            _Player->Print_Status();
+            break;
+        }
+
+        case 4:
         {
             if (_Player == nullptr)
             {
--- Text RPG/Game_Manager.h
+++ Text RPG/Game_Manager.h
@@ -6,6 +6,7 @@
 #include "Equipment.h"
 #include "Inventory.h"
 #include "Item.h"
+#include "Tutor.h"
 
 class Player;
 
@@ -27,5 +28,7 @@ private:
     Inventory<Item>    _Inventory;
     Inventory_For_Equipments_Only _Equipment_Inventory;
     Currently_Equipped_Equipments _Currently_Equipped_Equipments;
+    Currently_Equipped_Equipments _Currently_Equipped_Equipments;
+    Currently_Equipped_Tutor _Currently_Equipped_Tutor;
     Player* _Player;
 };
