ea40362331b3d685f503f59474e64f7391d63690
이미르 <2mir0012@gmail.com>
2026-08-04 15:35:32 +0900

    문제 설정 수정

--- Text RPG/Quiz_Bank.h
+++ Text RPG/Quiz_Bank.h
@@ -3,21 +3,83 @@
 #include <string>
 #include <vector>
 
+enum class Chapter_Type;
 
 struct Quiz
 {
-    std::string question;           // 문제 내용
-    std::vector<std::string> choices; // 4지선다 보기
-    int answer;                     // 정답 번호 (1 ~ 4)
+	std::string question;
+	std::vector<std::string> choices;
+	int answer;
+};
+
+struct Tutor_Question
+{
+	std::string description;
+	std::string code;
+	std::string correct_Answer;
 };
 
 
-extern std::vector<Quiz> EliteQuiz;
-extern std::vector<Quiz> VariableTutorQuiz;
-extern std::vector<Quiz> ArrayTutorQuiz;
-extern std::vector<Quiz> FunctionTutorQuiz;
-extern std::vector<Quiz> PointerTutorQuiz;
-extern std::vector<Quiz> ObjectTutorQuiz;
+enum class Final_Boss_Question_Type
+{
+	MULTIPLE_CHOICE,
+	SUBJECTIVE
+};
 
-extern std::vector<Quiz> KimDongHyunManagerQuiz;
-extern std::vector<Quiz> MoonSeungHoManagerQuiz;
+struct Final_Boss_Question
+{
+	Final_Boss_Question_Type type;
+	std::string description;
+	std::string code;
+	std::string choices[4];
+	int correct_Choice;
+	std::string correct_Answer;
+};
+
+extern std::vector<Quiz>
+Variable_Elite_Quiz;
+
+extern std::vector<Quiz>
+Array_Elite_Quiz;
+
+extern std::vector<Quiz>
+Function_Elite_Quiz;
+
+extern std::vector<Quiz>
+Pointer_Elite_Quiz;
+
+extern std::vector<Quiz>
+Object_Elite_Quiz;
+
+extern std::vector<Tutor_Question>
+Variable_Tutor_Quiz;
+
+extern std::vector<Tutor_Question>
+Array_Tutor_Quiz;
+
+extern std::vector<Tutor_Question>
+Function_Tutor_Quiz;
+
+extern std::vector<Tutor_Question>
+Pointer_Tutor_Quiz;
+
+extern std::vector<Tutor_Question>
+Object_Tutor_Quiz;
+
+extern std::vector<Quiz>
+KimDongHyunManagerQuiz;
+
+extern std::vector<Quiz>
+MoonSeungHoManagerQuiz;
+
+const std::vector<Quiz>* Get_Elite_Question_Bank(Chapter_Type chapter_Type);
+
+const std::vector<Tutor_Question>* Get_Tutor_Question_Bank(Chapter_Type chapter_Type);
+
+constexpr int FINAL_BOSS_QUESTION_COUNT = 10;
+
+void Get_Final_Boss_Questions(Final_Boss_Question final_Boss_Questions[]);
+
+std::vector<Final_Boss_Question>Create_Final_Boss_Objective_Pool();
+
+std::vector<Final_Boss_Question>Create_Final_Boss_Subjective_Pool();
