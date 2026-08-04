84e84e0e2d53ac2a8970df824a556e605073d264
hwa0907194-ui <hwa0907194@gmail.com>
2026-08-05 02:15:57 +0900

    사운드 매니저

---
+++ Text RPG/Sound_Manager.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+#include <string>
+
+enum class BGM_Type
+{
+    None,
+    Main,
+    Battle,
+    Boss
+};
+
+class Sound_Manager
+{
+private:
+    BGM_Type currentBGM;
+
+    Sound_Manager();
+
+    std::wstring Get_BGM_Path(BGM_Type bgmType) const;
+
+public:
+    // 복사 방지
+    Sound_Manager(const Sound_Manager&) = delete;
+    Sound_Manager& operator=(const Sound_Manager&) = delete;
+
+    // 프로그램 전체에서 같은 Sound_Manager 반환
+    static Sound_Manager& Get_Instance();
+
+    void Play_BGM(BGM_Type bgmType);
+    void Stop_BGM();
+
+    BGM_Type Get_Current_BGM() const;
+};
