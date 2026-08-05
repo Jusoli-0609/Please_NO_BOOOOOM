62155ec10af3f7e76647ae39ab63053f50de1ec6
hwa0907194-ui <hwa0907194@gmail.com>
2026-08-05 15:43:28 +0900

    Revert "일부 대사 수정"
    This reverts commit 4444a493179fd949ce0087a57c8e01adb95796d2.

--- Text RPG/JYJ.cpp
+++ Text RPG/JYJ.cpp
@@ -420,7 +420,7 @@ void JYJ::Groggy_Attack(Monster* monster)
     );
 
     std::cout << name << "의 " << groggyAttackName << "!\n";
-    Print_Bright_Dialogue("대신 해주십시요.\n");
+    Print_Bright_Dialogue("그로기 공격 대사 입력\n");
     // 명중한 공격에 치명타 판정 적용
     Apply_Critical_Damage(damage);
 
